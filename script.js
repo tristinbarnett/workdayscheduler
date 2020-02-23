e1506152e7b001ce5a206873aa1cf34742a6ec8d
Tristin Barnett <tristinbarnett@gmail.com>
2020-02-22 17:40:16 -0800

    updated local storage to create custom keys and use object array to retrieve correct keys from local storage. added lock to button

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 $(document).ready(function () {
     var currentHour = moment().hours();
+    var loopArray = [{
+        location: "firstBox",
+        hour: "9"
+    },{
+        location: "secondBox",
+        hour: "10"
+    },{
+        location: "thirdBox",
+        hour: "11"
+    },{
+        location: "fourthBox",
+        hour: "12"
+    },{
+        location: "fifthBox",
+        hour: "13"
+    },{
+        location: "sixthBox",
+        hour: "14"
+    },{
+        location: "seventhBox",
+        hour: "15"
+    },{
+        location: "eigthBox",
+        hour: "16"
+    },{
+        location: "ninthBox",
+        hour: "17"
+    }];
 
     function displayDate() {
         var date = moment().format("dddd, MMMM Do");
         $("#currentDay").html(date);
     }
     displayDate();
+    getItems();
 
     $(".timeRow").each(function () {
         var rowHour = parseInt($(this).attr("data-hour"));
@@ -25,11 +54,21 @@ $(document).ready(function () {
     })
 
 
- $(".saveBtn").click(function(){
-     var textLocation = $(this).attr("data-value");
-     console.log(textLocation);
-    var textInput = $(this).prev().val();
-    localStorage.setItem('textBox'+textLocation, JSON.stringify(textInput));
- })
+    $(".saveBtn").click(function () {
+        var textLocation = $(this).attr("data-value")
+        var textInput = $(this).prev().val();
+        localStorage.setItem(textLocation, JSON.stringify(textInput));
+    })
 
+
+    function getItems() {
+        console.log(loopArray);
+        for (i = 0; i < loopArray.length; i++) {
+            var localStorageItem = JSON.parse(localStorage.getItem(loopArray[i].location));
+            console.log(localStorageItem);
+            if (localStorageItem !== null) {
+                $("[data-hour=" + loopArray[i].hour + "]").text(localStorageItem);
+            }
+        }
+    }
 });
